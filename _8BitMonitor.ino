3949e4524a6bf74de0497dd8ac7f5a87d9df0c2a
Simon Arlott <sa.me.uk>
2013-05-08 18:39:51 +0100

    add debounce

--- _8BitMonitor.ino
+++ _8BitMonitor.ino
@@ -1,8 +1,9 @@
-#define INTERVAL 997
+#include <Bounce.h>
+#define INTERVAL 992
 #define DELAY 100
 
 int pins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
-int values[8] = { LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW };
+Bounce *values[8] = {};
 int report = false;
 unsigned long last_report;
 
@@ -11,8 +12,11 @@ void setup() {
   
   Serial.begin(115200);
   
-  for (i = 0; i < 8; i++)
+  for (i = 0; i < 8; i++) {
     pinMode(pins[i], INPUT);
+    values[i] = new Bounce(pins[i], 5);
+    values[i]->write(LOW);
+  }
   
   last_report = millis();
 }
@@ -24,30 +28,27 @@ void loop() {
 
   while (!Serial)
     last_report = millis();
-  
+
+  for (i = 0; i < 8; i++)
+    report |= values[i]->update();
+
   now = millis();
   elapsed = now - last_report;
-  
-  for (i = 0; i < 8; i++) {
-    tmp = digitalRead(pins[i]);
-    if (tmp != values[i])
-      report = true;
-    values[i] = tmp;
-  }
    
   if (elapsed >= INTERVAL || (report && elapsed >= DELAY)) {
     int value = 0;
-    
+
     for (i = 0; i < 8; i++)
-      if (values[i])
+      if (values[i]->read())
         value |= (1 << i);
-    
-    Serial.print("{ \"now\": ");
-    Serial.print(now);
-    Serial.print(", \"value\": ");
-    Serial.print(value);
-    Serial.println(" }");
-    Serial.flush();
+
+    if (Serial) {
+      Serial.print("{ \"now\": ");
+      Serial.print(now);
+      Serial.print(", \"value\": ");
+      Serial.print(value);
+      Serial.println(" }");
+    }
     
     last_report = now;
     report = false;
