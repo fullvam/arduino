3e15b1b16953336b4d47f0c2893297d7ea668eaf
Simon Arlott <sa.me.uk>
2013-05-17 00:13:19 +0100

    add copyright notice

--- lightswitch/lightswitch.ino
+++ lightswitch/lightswitch.ino
@@ -1,3 +1,21 @@
+/*
+	lightswitch - Apply pulses to light switch impulse latching relays from serial input
+	Copyright 2013  Simon Arlott
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 enum state {
   idle,
   pulse_on,
